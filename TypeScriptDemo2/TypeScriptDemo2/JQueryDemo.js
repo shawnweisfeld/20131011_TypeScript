e5a45220783bb2708ea03c9966ec71ca1c309c51
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-10-10 22:27:31 -0500

    upgraded samples for VS 2013 RC

---
+++ TypeScriptDemo2/TypeScriptDemo2/JQueryDemo.js
@@ -0,0 +1,7 @@
+/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+$(function () {
+    $('#btn').click(function () {
+        alert("hello from jquery");
+    });
+});
+//# sourceMappingURL=JQueryDemo.js.map
---
+++ TypeScriptDemo2/TypeScriptDemo2/JQueryDemo.ts
@@ -0,0 +1,9 @@
+/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+
+$(function () {
+    $('#btn').click(function () {
+        alert("hello from jquery");
+    });
+
+
+})
